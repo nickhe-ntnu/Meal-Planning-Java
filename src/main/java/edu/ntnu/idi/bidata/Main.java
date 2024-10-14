5bd469450391289beb6247ad9009daf5ee614ca0
Nick Heggø <nickhe@stud.ntnu.no>
2024-10-14 14:17:08 +0200

    test git

--- src/main/java/edu/ntnu/idi/bidata/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/Main.java
@@ -5,4 +5,7 @@ package edu.ntnu.idi.bidata;
  * instance of your main-class that starts your application.
  */
 public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hellow world");
+  }
 }
