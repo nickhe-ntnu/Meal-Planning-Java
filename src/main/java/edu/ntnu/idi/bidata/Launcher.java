c2d966af50145e1d348cc3d43574e5d6062bb659
Nick Heggø <nickhe@stud.ntnu.no>
2025-01-13 22:14:07 +0100

    Added the startup option to choose between the simple and advanced mode

--- src/main/java/edu/ntnu/idi/bidata/Launcher.java
+++ src/main/java/edu/ntnu/idi/bidata/Launcher.java
@@ -1,6 +1,9 @@
 package edu.ntnu.idi.bidata;
 
+import java.util.Scanner;
+
 import edu.ntnu.idi.bidata.user.User;
+import edu.ntnu.idi.bidata.util.Utility;
 
 /**
  * The Launcher class serves as the entry point for the meal planning application.
@@ -14,12 +17,26 @@ public class Launcher {
   private Launcher() {
   }
 
+  private static boolean isAdvancedModeOn() {
+    String input;
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.println("Do you wish to initialize the advanced mode? (Y/n)");
+      input = scanner.nextLine();
+    }
+    return Utility.isContinuationConfirmed(input);
+  }
+
   /**
    * The main entry point of the application. Instantiates and runs the Application.
    */
   public static void main(String[] args) {
+    Application app;
     User user = new User();
-    Application app = new AdvancedApplication(user);
+    if (isAdvancedModeOn()) {
+      app = new AdvancedApplication(user);
+    } else {
+      app = new SimpleApplication(user);
+    }
     app.run();
   }
 
